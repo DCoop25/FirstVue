598805d3f6bc4adab688b24e68c9fd5652e79100
Dylan Rongey <dylan.rongey@gmail.com>
2019-10-11 11:20:45 -0500

    Adding original files

---
+++ adder.js
@@ -0,0 +1,19 @@
+const add = (x, y) => { return x + y }
+
+
+const calculateFarenheit = (x) => { return x * 9.00/5.0 + 32.0 }
+
+const adder = new Vue({
+  el: '#adder',
+  data: {
+    guest: 'Emmett',
+    firstNumber: 5,
+  },
+  computed: {
+    result: function () {
+      const i = parseInt(this.firstNumber)
+
+      return `${this.guest}, your sum is ${calculateFarenheit(i)}.`
+    }
+  }
+})
